d5596c14ed7d2cae6e593893df01bf81c0bfe9f9
Jackie Hu <41136700+Jackie-Who@users.noreply.github.com>
2025-12-07 00:07:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,14 +18,6 @@ build_exe_options = {
     ],
     "include_files": [
         "icon.ico",
-        "icon.png",
-        "Font Awesome 7 Free-Solid-900.otf",
-        ("Icons", "Icons"),
-        ("Modulos", "Modulos"),
-        ("Module-Effects", "Module-Effects"),
-        "custom_presets.json",
-        "Fix Names.json", # Asegurarse de incluir este archivo de datos
-        "Back.webp", # Asegurarse de incluir esta imagen
         "LICENSE", # Incluir la licencia
         "README.md", # Incluir el README
     ],
@@ -41,7 +33,8 @@ if sys.platform == "win32":
 setup(
     name="BPSR Module Optimizer",
     version="1.0",
-    description="BPSR Module Optimizer by: MrSnake",
+    description="BPSR Module Optimizer by: Jackie",
     options={"build_exe": build_exe_options},
-    executables=[Executable("gui_app.py", base=base, icon="icon.ico")],
+    executables=[Executable("simple_gui.py", base=base, icon="icon.ico")],
 )
+
